d2d5c6c5940e7215af77991066b425bb51fad5b2
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2022-07-18 21:26:33 +0500

    portfolio

---
+++ practice.js
@@ -0,0 +1,7 @@
+
+var name = prompt("Place it in html")
+document.getElementById("thisone").innerHTML = "HELLOO"
+document.getElementById("thisone").setAttribute("src","./Assignnment/img/hamzaAsaad.jpeg")
+
+console.log(
+    document.getElementById("thisone"))
