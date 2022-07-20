c6844d75c9be17d9aee83144be3db5efb77d376f
Shehreyar Kitchlew <62876048+Kitchlew@users.noreply.github.com>
2022-07-20 15:34:13 +0500

    new branch

--- Portfolio/newModule.js
+++ Portfolio/newModule.js
@@ -1,145 +1,100 @@
 
+		
 			import * as THREE from 'three';
 
-			import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-			import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-			import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-			import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass.js';
-			import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js';
+			import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
-			let container;
 
-			let camera, scene, renderer;
+	
 
-			let video, texture, material, mesh;
+			let scene, camera, renderer;
+			let geometry, mesh, material;
+			let mouse, center;
 
-			let composer;
-
-			let mouseX = 0;
-			let mouseY = 0;
-
-			let windowHalfX = window.innerWidth / 2;
-			let windowHalfY = window.innerHeight / 2;
-
-			let cube_count;
-
-			const meshes = [],
-				materials = [],
-
-				xgrid = 20,
-				ygrid = 10;
-
-			const startButton = document.getElementById( 'startButton' );
-			startButton.addEventListener( 'click', function () {
-
-				init();
-				animate();
-
-			} );
+			init();
+			animate();
 
 			function init() {
 
-				const overlay = document.getElementById( 'overlay' );
-				overlay.remove();
-
-				container = document.createElement( 'div' );
+				const container = document.createElement( 'div' );
 				document.body.appendChild( container );
 
-				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-				camera.position.z = 500;
+				const info = document.createElement( 'div' );
+				info.id = 'info';
+				info.innerHTML = '<a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> - kinect';
+				document.body.appendChild( info );
+
+				camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
+				camera.position.set( 0, 0, 500 );
 
 				scene = new THREE.Scene();
+				center = new THREE.Vector3();
+				center.z = - 1000;
 
-				const light = new THREE.DirectionalLight( 0xffffff );
-				light.position.set( 0.5, 1, 1 ).normalize();
-				scene.add( light );
+				const video = document.getElementById( 'video' );
+
+				const texture = new THREE.VideoTexture( video );
+				texture.minFilter = THREE.NearestFilter;
+
+				const width = 640, height = 480;
+				const nearClipping = 850, farClipping = 4000;
+
+				geometry = new THREE.BufferGeometry();
+
+				const vertices = new Float32Array( width * height * 3 );
+
+				for ( let i = 0, j = 0, l = vertices.length; i < l; i += 3, j ++ ) {
+
+					vertices[ i ] = j % width;
+					vertices[ i + 1 ] = Math.floor( j / width );
+
+				}
+
+				geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+
+				material = new THREE.ShaderMaterial( {
+
+					uniforms: {
+
+						'map': { value: texture },
+						'width': { value: width },
+						'height': { value: height },
+						'nearClipping': { value: nearClipping },
+						'farClipping': { value: farClipping },
+
+						'pointSize': { value: 2 },
+						'zOffset': { value: 1000 }
+
+					},
+					vertexShader: document.getElementById( 'vs' ).textContent,
+					fragmentShader: document.getElementById( 'fs' ).textContent,
+					blending: THREE.AdditiveBlending,
+					depthTest: false, depthWrite: false,
+					transparent: true
+
+				} );
+
+				mesh = new THREE.Points( geometry, material );
+				scene.add( mesh );
+
+				const gui = new GUI();
+				gui.add( material.uniforms.nearClipping, 'value', 1, 10000, 1.0 ).name( 'nearClipping' );
+				gui.add( material.uniforms.farClipping, 'value', 1, 10000, 1.0 ).name( 'farClipping' );
+				gui.add( material.uniforms.pointSize, 'value', 1, 10, 1.0 ).name( 'pointSize' );
+				gui.add( material.uniforms.zOffset, 'value', 0, 4000, 1.0 ).name( 'zOffset' );
+
+				video.play();
+
+				//
 
 				renderer = new THREE.WebGLRenderer();
 				renderer.setPixelRatio( window.devicePixelRatio );
 				renderer.setSize( window.innerWidth, window.innerHeight );
 				container.appendChild( renderer.domElement );
 
-				video = document.getElementById( 'video' );
-				video.play();
-				video.addEventListener( 'play', function () {
-
-					this.currentTime = 3;
-
-				} );
-
-				texture = new THREE.VideoTexture( video );
-
-				//
-
-				let i, j, ox, oy, geometry;
-
-				const ux = 1 / xgrid;
-				const uy = 1 / ygrid;
-
-				const xsize = 480 / xgrid;
-				const ysize = 204 / ygrid;
-
-				const parameters = { color: 0xffffff, map: texture };
-
-				cube_count = 0;
-
-				for ( i = 0; i < xgrid; i ++ ) {
-
-					for ( j = 0; j < ygrid; j ++ ) {
-
-						ox = i;
-						oy = j;
-
-						geometry = new THREE.BoxGeometry( xsize, ysize, xsize );
-
-						change_uvs( geometry, ux, uy, ox, oy );
-
-						materials[ cube_count ] = new THREE.MeshLambertMaterial( parameters );
-
-						material = materials[ cube_count ];
-
-						material.hue = 1;
-						material.saturation = 1;
-
-						material.color.setRGB( material.hue, material.saturation, 1
-                             );
-
-						mesh = new THREE.Mesh( geometry, material );
-
-						mesh.position.x = ( i - xgrid / 2 ) * xsize;
-						mesh.position.y = ( j - ygrid / 2 ) * ysize;
-						mesh.position.z = 0;
-
-						mesh.scale.x = mesh.scale.y = mesh.scale.z = 1;
-
-						scene.add( mesh );
-
-						mesh.dx = 0.001 * ( 0.5 - Math.random() );
-						mesh.dy = 0.001 * ( 0.5 - Math.random() );
-
-						meshes[ cube_count ] = mesh;
-
-						cube_count += 1;
-
-					}
-
-				}
-
-				renderer.autoClear = false;
+				mouse = new THREE.Vector3( 0, 0, 1 );
 
 				document.addEventListener( 'mousemove', onDocumentMouseMove );
-
-				// postprocessing
-
-				const renderModel = new RenderPass( scene, camera );
-				const effectBloom = new BloomPass( 1.3 );
-				const effectCopy = new ShaderPass( CopyShader );
-
-				composer = new EffectComposer( renderer );
-
-				composer.addPass( renderModel );
-				composer.addPass( effectBloom );
-				composer.addPass( effectCopy );
 
 				//
 
@@ -149,39 +104,19 @@
 
 			function onWindowResize() {
 
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
-
 				camera.aspect = window.innerWidth / window.innerHeight;
 				camera.updateProjectionMatrix();
 
 				renderer.setSize( window.innerWidth, window.innerHeight );
-				composer.setSize( window.innerWidth, window.innerHeight );
 
 			}
-
-			function change_uvs( geometry, unitx, unity, offsetx, offsety ) {
-
-				const uvs = geometry.attributes.uv.array;
-
-				for ( let i = 0; i < uvs.length; i += 2 ) {
-
-					uvs[ i ] = ( uvs[ i ] + offsetx ) * unitx;
-					uvs[ i + 1 ] = ( uvs[ i + 1 ] + offsety ) * unity;
-
-				}
-
-			}
-
 
 			function onDocumentMouseMove( event ) {
 
-				mouseX = ( event.clientX - windowHalfX );
-				mouseY = ( event.clientY - windowHalfY ) * 0.3;
+				mouse.x = ( event.clientX - window.innerWidth / 2 ) * 8;
+				mouse.y = ( event.clientY - window.innerHeight / 2 ) * 8;
 
 			}
-
-			//
 
 			function animate() {
 
@@ -191,59 +126,14 @@
 
 			}
 
-			let h, counter = 1;
-
 			function render() {
 
-				const time = Date.now() * 0.00005;
+				camera.position.x += ( mouse.x - camera.position.x ) * 0.05;
+				camera.position.y += ( - mouse.y - camera.position.y ) * 0.05;
+				camera.lookAt( center );
 
-				camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-				camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
-
-				camera.lookAt( scene.position );
-
-				for ( let i = 0; i < cube_count; i ++ ) {
-
-					material = materials[ i ];
-
-					h = 1;
-					material.color.setRGB( 1, 1, 1);
-
-				}
-
-				if ( counter % 1000 > 200 ) {
-
-					for ( let i = 0; i < cube_count; i ++ ) {
-
-						mesh = meshes[ i ];
-
-						mesh.rotation.x += 10 * mesh.dx;
-						mesh.rotation.y += 10 * mesh.dy;
-
-						mesh.position.x -= 150 * mesh.dx;
-						mesh.position.y += 150 * mesh.dy;
-						mesh.position.z += 300 * mesh.dx;
-
-					}
-
-				}
-
-				if ( counter % 1000 === 0 ) {
-
-					for ( let i = 0; i < cube_count; i ++ ) {
-
-						mesh = meshes[ i ];
-
-						mesh.dx *= - 1;
-						mesh.dy *= - 1;
-
-					}
-
-				}
-
-				counter ++;
-
-				renderer.clear();
-				composer.render();
+				renderer.render( scene, camera );
 
 			}
+
+	
